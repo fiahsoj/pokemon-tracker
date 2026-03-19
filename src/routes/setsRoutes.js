dca08f165a45f7c35b92db3b5e58ab4c38eaacfa
fiahsoj <matthew.cham@hotmail.com>
2026-03-19 15:40:55 +1100

    feat: added percentage completion route

--- src/routes/setsRoutes.js
+++ src/routes/setsRoutes.js
@@ -14,6 +14,16 @@ router.get("/:name", getSet)
 
 router.post("/:name/collect", collectCards)
 
-router.post("/:name/completion", getCompletionPercentage)
+router.post("/users/:userId/sets/:setId/completion", getCompletionPercentage)
+
+app.get("/users/:userId/sets/:setId/completion", (req, res) => {
+  const userId = Number(req.params.userId)
+  const setId = Number(req.params.setId)
+  const completion = getCompletionPercentage(userId,setId)
+  
+  res.json({
+    completion: completion
+  })
+})
 
 export default router
