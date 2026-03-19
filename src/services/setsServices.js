076ae533916fffbe0f05d58e4def1491eca484a8
fiahsoj <matthew.cham@hotmail.com>
2026-03-19 15:11:12 +1100

    feat:added completion percentage function

--- src/services/setsServices.js
+++ src/services/setsServices.js
@@ -1,3 +1,5 @@
+import { collectCards } from "../controllers/setsController.js"
+import collections from "../data/collections.js"
 import { sets } from "../data/sets.js"
 
 export function getAllSets() {
@@ -20,18 +22,21 @@ export function updateCollected(name, collected) {
   return set
 }
 
-export function getCompletionPercentage(name){
-    const set = getSetByName(name)
+export function getCompletionPercentage(userId,setId){
+    const userCards = collections.filter(item => item.userId === userId)
+    const cardsInSet = cards.filter(card => card.setId === setId)
+    const ownedCardsIds = userCards.map(item => item.cardId)
+    const ownedCardSet = new Set(ownedCardIds)
 
-    if (!set) return null
+    let ownedCount = 0
 
-    const totalCards = set.totalCards
-
-    const ownedCards = userCards[name] || []
-
-    const ownedCount = ownedCards.length
-
-    const percentage = (ownedCount/totalCards) * 100
-
-    return percentage
+    for (const card of cardsInSet){
+      if (ownedCardSet.has(cardsInSet)) {
+        ownedCount++
+      }
+    }
+    const completionPercentage = (ownedCount / cardsInSet.length) * 100
+    const roundedPercentage = Number(completionPercentage.toFixed(2))
+    
+    return completionPercentage
 }
